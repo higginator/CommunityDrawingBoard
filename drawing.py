3422f7dc4b2886295e325e2d0df6bb93fe132fa5
Ryan Higgins <ryanhigs@gmail.com>
2018-01-31 17:14:51 -0800

    HTML templates environment setup

--- drawing.py
+++ drawing.py
@@ -1,9 +1,24 @@
 import webapp2
+import jinja2
+import os
+
+# setup HTML template loader
+template_env = jinja2.Environment(
+loader=jinja2.FileSystemLoader(os.getcwd() + '/templates'))
+
 
 class Draw(webapp2.RequestHandler):
     def get(self):
-        self.response.out.write('drawing')
+        # HTML template
+        template = template_env.get_template('drawing.html')
 
+        # variable
+        context = {}
+
+        # response
+        self.response.out.write(template.render(context))
+
+# request routing
 application = webapp2.WSGIApplication([
                                     ('/', Draw)
                                     ], debug=True)
